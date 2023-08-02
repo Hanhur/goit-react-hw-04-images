0a358afd1afb379897661ceda33721584a8affb5
Vladimir Hanhur <aqgz@email.cz>
2023-08-02 15:33:49 +0200

    update

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ButtonStyled } from './Button.styled';
+import PropTypes from 'prop-types';
+
+const Button = ({ onClick }) => {
+    return (
+        <ButtonStyled type="button" onClick={onClick}>Load more</ButtonStyled>
+    );
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+}
+
+export default Button;
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import { ImageGalleryStyled } from './ImageGallery.styled';
+
+const ImageGallery = ({ images, onOpenModal }) => {
+    return (
+        <div>
+            <ImageGalleryStyled>
+                {images.map(image => (
+                    <ImageGalleryItem
+                        key={image.id}
+                        image={image}
+                        onOpenModal={onOpenModal}/>
+                ))}
+            </ImageGalleryStyled>
+        </div>
+    );
+}
+
+ImageGallery.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+    onOpenModal: PropTypes.func.isRequired,
+}
+
+export default ImageGallery;
